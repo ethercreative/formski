50834a02a540ec6e43c59be496cba2318aecb168
Tam <hi@tam.sx>
2018-09-17 13:37:11 +0100

    Basic setup

---
+++ src/Formski.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Formski for Craft CMS
+ *
+ * @link      https://ethercreative.co.uk
+ * @copyright Copyright (c) 2018 Ether Creative
+ */
+
+namespace ether\formski;
+
+use craft\base\Plugin;
+
+/**
+ * Class Formski
+ *
+ * @author  Ether Creative
+ * @package ether\formski
+ */
+class Formski extends Plugin
+{
+
+	public function init ()
+	{
+		parent::init();
+
+		// ...
+	}
+
+}
